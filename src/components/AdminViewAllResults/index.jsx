55f5cac0c1d3c0d69de9aad062f47561c2c78b5d
Soumya <gurramsoumyadevender@gmail.com>
2023-08-10 19:58:43 +0530

    admin view all results-Integration

--- src/components/AdminViewAllResults/index.jsx
+++ src/components/AdminViewAllResults/index.jsx
@@ -1,34 +1,247 @@
 import React, { useState,useRef } from "react";
 import "./index.css";
-import { FormControl,MenuItem,Select,NativeSelect,InputLabel,Box,Table, TableRow,TableHead,TableCell, TableBody,Button, Grid, Stack} from "@mui/material";
+import { FormControl,MenuItem,Select,NativeSelect,InputLabel,Box,Table, TableRow,TableHead,TableCell, TableBody,Button, Grid, Stack, Dialog, DialogContent,DialogActions, TableContainer, Divider} from "@mui/material";
+import axios from "axios";
 
 
-
-
+let testname;
+let testcode;
+let className;
+let acYear;
+let c_code;
+let resultdetails=[];
 const AdminViewAllResults=()=>
 {
+    const [classes, setClasses]=useState([]);
     const [classCode, setClassCode]=useState('');
+    const [testList,setTestList]=useState([]);
     const [test,setTest]=useState("");
+    const [testCode,setTestCode]=useState("");
     const [year,setYear]=useState('');
+    const [results,setResults]=useState([]);
+    const [selectedclass,setSelectedclass]=useState('');
+    const [academicYears,setAcademicYears]=useState([]);
+    const [maxExamMarks,setMaxExamMarks]=useState([]);
+    const [maxTestMarks,setMaxTestMarks]=useState([]);
     const [view,setView]=useState('');
+    const [subjects,setSubjects]=useState([]);
+    const [classError, setClassError] = useState("");
+    const [testError,setTestError]=useState("");
+    const [yearError,setYearError]=useState("");
+    const [disableClass,setDisableClass]=useState(true);
+    const [disableYear,setDisableYear]=useState(true);
+  
+   
     
-   
-   
+    let testType;
+    let subschedule;
+    let subcode;
+    let resultInfo=[];
+    let students=[];
+    React.useEffect(()=>{
+      getClassList();
+      setView('');
+      
+  },[]);
     const handleClassNameSelect=(event)=>
     { 
       setClassCode(event.target.value);
+      c_code=event.target.value;
+      const subjectarray=getSubjects(c_code);
+      setDisableClass(false);
+      getAcademicYears(c_code);
     }
+    
     const handleTestNameSelect=(event)=>
     {
       setTest(event.target.value);
+      testname=event.target.value;
+     
+        axios.get(`http://localhost:8080/schedule/v1/viewSchedule?scheduleName=${testname}&className=${className}&acYear=${acYear}`)
+       .then(result =>
+        {
+          setTestCode(result?.data.scheduleCode)
+          testcode=result?.data.scheduleCode
+          testType=result?.data.scheduleType
+          subschedule=result?.data.subjectSchedule;
+          getSubMaxMarks(subschedule,testType)
+          console.log(testname+" "+testType,subschedule);
+    } )
+       .catch(err=>{
+         console.log(err.message)
+       })
+    }
+    const getSubMaxMarks=(subschedule,testType)=>
+    {
+      subschedule.map((subject,index)=>{
+      subcode=subject.subjectCode
+       axios.get(`http://localhost:8080/subjects/v1/subject/${subcode}`)
+       .then(res=>{
+         console.log(res?.data)
+        if(testType==="Exam")
+        {
+          setMaxTestMarks(current => [...current,0])
+          setMaxExamMarks(current => [...current, res?.data.maxExamMarks]);
+          
+          
+        }
+        else if(testType==="Test")
+        {
+          setMaxTestMarks(current => [...current, res?.data.maxTestMarks]);
+          setMaxExamMarks(current => [...current, 0]);
+        }
+        })
+      }
+      )
     }
     const handleYearSelect=(event)=>
     {
       setYear(event.target.value);
+      acYear=event.target.value;
+      getTestList(className,acYear);
+      setDisableYear(false);
+      console.log(acYear)
     }
-    const handleView=(str)=>
+    const addstudentInfo=(resultdetails)=>
     {
-      setView(str)
+      
+      axios.get(`http://localhost:8080/student/v1/student/${c_code}`)
+        .then(res=>{
+          students=res?.data;
+          console.log(res?.data)
+          resultInfo= resultdetails.map((result,index)=>
+          {
+             const studentDetails = students.find(student => student.studentId === result.studentId)
+             result.studentName =studentDetails? studentDetails.name: null 
+             return result
+          })
+          setResults(resultdetails)
+          console.log(resultInfo);
+          console.log(resultdetails);
+        })
+        .catch(err=>{
+          console.log(err.message)
+        })
+    }
+    const getresultdetails=async()=>
+     {
+       const classNameValidation=validateclass();
+       const yearValidation=validateYear();
+       const testNameValidation=validateTest();
+
+     if(classNameValidation||yearValidation||testNameValidation)
+  {
+    if(classNameValidation===yearValidation&&yearValidation===testNameValidation)
+    {
+      setClassError("Please select class, year and test to view results");
+      setYearError("");
+      setTestError("");
+    }
+    else if(classNameValidation===testNameValidation)
+    {
+      setClassError("Please select class and test")
+      setTestError("");
+    }
+     else if(yearValidation===testNameValidation)
+     {
+      setYearError("Please select year and test")
+      setTestError("");
+     }
+  }
+     else
+     {
+     await axios.get(`http://localhost:8080/results/v1/classTest?classCode=${c_code}&academicYear=${acYear}&scheduleCode=${testCode}`)
+    .then(res=>
+      {
+        resultdetails=res?.data;
+        console.log(resultdetails);
+        addstudentInfo(resultdetails)
+      })
+    .catch(err=>{
+      console.log(err.message)
+    })
+    setView("yes")
+  }
+  
+    }
+    const getSubjects=async(classCode)=>
+    {
+      await axios.get(`http://localhost:8080/classes/v1/classes/${classCode}`)
+      .then(
+        result =>
+        {
+          setSubjects(result?.data.subjects)
+          className=result?.data.name;
+          setSelectedclass(className)
+          console.log(result?.data.subjects)
+          return result?.data.subjects;
+        }
+        
+        )
+      .catch(err=>{
+        console.log(err.message)
+      })
+    }
+   
+    const getClassList = async()=>
+    {
+      await axios.get("http://localhost:8080/classes/v1/classes")
+      .then(result =>setClasses(result?.data))
+      .catch(err=>{
+        console.log(err.message)
+      })
+    }
+
+    const getTestList = async(className,acYear)=>
+    {
+      await axios.get(`http://localhost:8080/schedule/v1/scheduleNames?className=${className}&acYear=${acYear}`)
+      .then(result =>{setTestList(result?.data)
+        return  result?.data}
+        )
+      
+      .catch(err=>{
+        console.log(err.message)
+      })
+    }
+    const getAcademicYears=async(classCode)=>
+    {
+      await axios.get(`http://localhost:8080/schedule/v1/${classCode}/acYears`)
+      .then(result=>{setAcademicYears(result?.data)
+      console.log(result?.data)})
+      .catch(err=>{
+        console.log(err.message)
+      })
+    }
+    const validateclass = () => {
+      if (selectedclass === "") {
+        setClassError("Please select a class");
+        return true;
+      } else {
+        setClassError("");
+        return false;
+      }
+    };
+    const validateTest = () => {
+      if (test === "") {
+        setTestError("Please select a test");
+        return true;
+      } else {
+        setTestError("");
+        return false;
+      }
+    };
+    const validateYear=()=>
+    {
+      if(year==="")
+      {
+        setYearError("Please select a year");
+        return true;
+      }
+      else
+      {
+        setYearError("");
+        return false;
+      }
     }
     return(
     <div>
@@ -40,105 +253,120 @@ const AdminViewAllResults=()=>
         <Stack direction="row" justifyContent="space-between">
       <Box className="class-drop-down">
       <FormControl fullWidth variant="filled" sx={{ m: 1 }} >
-               <InputLabel >Class Name</InputLabel>
-               <Select onChange={handleClassNameSelect}>
-                    <MenuItem value={1}>1</MenuItem>
-                    <MenuItem value={2}>2</MenuItem>
-                    <MenuItem value={3}>3</MenuItem>
-                    <MenuItem value={4}>4</MenuItem>
-                    <MenuItem value={5}>5</MenuItem>
-                    <MenuItem value={6}>6</MenuItem>
+               <InputLabel >Class</InputLabel>
+               <Select required={true} onChange={handleClassNameSelect}>
+                {classes?.map((classes,id) => (
+                    <MenuItem key={id} value={classes.code}>{classes.name}</MenuItem>
+                        ))}
                 </Select>
             </FormControl>
          </Box>
          <Box className="year-drop-down">
                <FormControl fullWidth variant="filled" >
                <InputLabel>Academic Year</InputLabel>
-                <Select onChange={handleYearSelect}>
-                    <MenuItem value={"2023-2024"}>2023-2024</MenuItem>
-                    <MenuItem value={"2022-2023"}>2022-2023</MenuItem>
-                    <MenuItem value={"2021-2022"}>2021-2022</MenuItem>
-                    <MenuItem value={"2020-2021"}>2020-2021</MenuItem>
-                    <MenuItem value={"2019-2020"}>2019-2020</MenuItem>
-                    <MenuItem value={"2018-2019"}>2018-2019</MenuItem>
-         
+                <Select value={year} 
+                          onChange={handleYearSelect} disabled={disableClass}>
+                            {academicYears.map((years,index)=>
+                            (
+                              <MenuItem value={years}>{years}</MenuItem>
+                            ))}
                 </Select>
                </FormControl>
          </Box>
          <Box className="test-drop-down">
                <FormControl fullWidth variant="filled" >
-               <InputLabel>Test/Exam Name</InputLabel>
-               <Select  onChange={handleTestNameSelect}>
-                    <MenuItem value={"Test 1"}>Test 1</MenuItem>
-                    <MenuItem value={"Test 2"}>Test 2</MenuItem>
-                    <MenuItem value={"Test 3"}>Test 3</MenuItem>
-                    <MenuItem value={"Test 4"}>Test 4</MenuItem>
-                    <MenuItem value={"Exam 1"}>Exam 1</MenuItem>
-                    <MenuItem value={"Exam 2"}>Exam 2</MenuItem>
+               <InputLabel>Test/Exam</InputLabel>
+                <Select disabled={disableClass||disableYear} onChange={handleTestNameSelect}>
+                            {testList?.map((test,id) => (
+                      <MenuItem key={id} value={test}>{test}</MenuItem>
+                      ))}
+
                 </Select>
                </FormControl>
          </Box>
          <Box>
                <FormControl  variant="filled" >
-               <Button variant="contained" className="view-button" onClick={()=>handleView("yes")} >view</Button>
+               <Button variant="contained" className="view-button" onClick={getresultdetails} >view</Button>
                </FormControl>
          </Box>
          </Stack>
          </Box>
+         <div>
+                    {classError && (
+                      <span style={{ color: "red" }}>{classError}</span>
+                    )}
+                    {yearError&&(
+                      <span style={{color: "red"}}>{yearError}</span>
+                    )}
+                    {testError && (
+                      <span style={{ color: "red" }}>{testError}</span>
+                    )}
+          </div>
        {view==="yes"?
+       <div className="table-block">
+        {resultdetails.length!=0?
        <Box>
+        <div className="tableContainer">
        <Table className="Results-table">
            <TableHead className="Table-head">
-           <TableRow>
-           <TableCell className="Head-Table-cell" rowSpan={2}  >Student ID</TableCell>
-                 <TableCell className="Head-Table-cell" rowSpan={2} >Student Name</TableCell>
-                 <TableCell className="Head-Table-cell" colSpan={2}>Subject1</TableCell>
-                 <TableCell className="Head-Table-cell" colSpan={2}>Subject2</TableCell>
-                 <TableCell className="Head-Table-cell" colSpan={2}>Subject3</TableCell>
-                 <TableCell className="Head-Table-cell" rowSpan={2} >FinalMarks</TableCell>
-                 <TableCell className="Head-Table-cell" rowSpan={2}>Percentage</TableCell>
-           </TableRow>
-           <TableRow>
-             <TableCell className="Head-Table-cell">IM</TableCell>
-               <TableCell className="Head-Table-cell">EM</TableCell>
-               <TableCell className="Head-Table-cell">IM</TableCell>
-               <TableCell className="Head-Table-cell">EM</TableCell>
-               <TableCell className="Head-Table-cell">IM</TableCell>
-               <TableCell className="Head-Table-cell">EM</TableCell>
-               </TableRow>
+               <TableCell className="Head-Table-cell" >Student ID </TableCell>
+               <TableCell className="Head-Table-cell"  >Student Name</TableCell>
+               {subjects?.map((sub,index) => (
+                      <TableCell className="Head-Table-cell"  >
+                        <div>
+                        {sub} 
+                        <Divider style={{backgroundColor:"black"}}></Divider>
+                        <Stack direction="row">
+                        <div style={{padding:"5px",paddingLeft:"25px",alignContent:"center",paddingBottom:"0px"}} className="Head-Table-cell">
+                          IM
+                          <div> {maxTestMarks[index]}</div>
+                        </div>
+                        <Divider orientation="vertical" flexItem style={{backgroundColor:"black", marginLeft:"15px", paddingBottom:"50px"}}></Divider>
+                        <div style={{padding:"5px",paddingLeft:"25px",alignContent:"center", paddingBottom:"0px"}} className="Head-Table-cell">
+                          EM
+                          <div> {maxExamMarks[index]}</div>
+                          </div>
+                        </Stack>
+                        </div>
+                      </TableCell>
+                      ))}
+
+               <TableCell className="Head-Table-cell"  >FinalMarks</TableCell>
+               <TableCell className="Head-Table-cell"  >Percentage</TableCell>
            </TableHead>
            <TableBody>
-           <TableRow>
-              <TableCell>1</TableCell>
-              <TableCell>Ravi</TableCell>
-              <TableCell>20</TableCell>
-              <TableCell>75</TableCell>
-              <TableCell>18</TableCell>
-              <TableCell>60</TableCell>
-              <TableCell>20</TableCell>
-              <TableCell>75</TableCell>
-              <TableCell>356</TableCell>
-              <TableCell>89</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>2</TableCell>
-              <TableCell>Krishna</TableCell>
-              <TableCell>20</TableCell>
-              <TableCell>75</TableCell>
-              <TableCell>18</TableCell>
-              <TableCell>60</TableCell>
-              <TableCell>20</TableCell>
-              <TableCell>75</TableCell>
-              <TableCell>356</TableCell>
-              <TableCell>89</TableCell>
-            </TableRow>
+  
+           {resultdetails?.map((result, index) => {
+  return (
+    <TableRow key={index}>
+      <TableCell className="Table-cell">{result.studentId}</TableCell>
+      <TableCell className="Table-cell">{result.studentName}</TableCell>
+      {result.marksList?.map((marks,index)=>
+      {return(
+        <TableCell>
+          <Stack direction="row">
+          <div style={{padding:"5px",paddingLeft:"25px"}} className="Table-cell">{marks.internalMarks}</div>
+          <Divider orientation="vertical" flexItem style={{backgroundColor:"black", marginLeft:"15px"}}></Divider>
+          <div style={{padding:"5px",paddingLeft:"25px"}} className="Table-cell">{marks.externalMarks}</div>
+          </Stack>
+        </TableCell>
+           );})}
+        <TableCell className="Table-cell">{result.totalMarks}</TableCell>
+        <TableCell className="Table-cell">{result.totalPercentage}</TableCell>
+    </TableRow>
+  );
+})}
+
+
            </TableBody>
        </Table>
+       </div>
        </Box>
+     :<p style={{textAlign:"center", color:"red"}}>Results not found</p>}
+       </div>
        :
-       <p>Note: select classname, testname, academic year to view test results</p>
+       <p style={{textAlign:"center"}}>Note: select classname, academic year and testname to view test results</p>
        }
-        
 
        </div>
     </div>
@@ -147,3 +375,4 @@ const AdminViewAllResults=()=>
 }
 
 export default AdminViewAllResults;
+
