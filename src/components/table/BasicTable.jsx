7b0df80b9ca3d270389a35d3a08c79504b7c98e6
Varshini R <varshiniradha2001@google.com>
2023-08-15 13:23:26 +0530

    CSS and Validations for Leaderboard

--- src/components/table/BasicTable.jsx
+++ src/components/table/BasicTable.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 import { DatePicker, TimePicker } from "@mui/lab";
-import { TextField } from "@mui/material";
+// import { TextField } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
